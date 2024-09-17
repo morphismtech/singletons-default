541bf08889bd1c73b96e9d9b831d4a34538da690
Eitan Chatav <eitan.chatav@gmail.com>
2024-09-16 20:14:38 -0700

    docs

--- src/Data/Default/Singletons.hs
+++ src/Data/Default/Singletons.hs
@@ -1,3 +1,85 @@
+{-|
+Module      : Data.Default.Singletons
+Description : Provides singleton-based default values and optional types.
+Copyright   : (c) 2024, Eitan Chatav
+License     : MIT
+Maintainer  : eitan.chatav@gmail.com
+Stability   : experimental
+Portability : non-portable (GHC extensions)
+
+This module defines an `Opt`ional type with
+either a `Def`ault value,
+or `Some` specific value.
+
+>>> definite (Def :: Opt True)
+True
+>>> definite (Some False :: Opt True)
+False
+>>> definite (Some True :: Opt True)
+True
+>>> maybe "def" show (perhaps (Def :: Opt True))
+"def"
+>>> maybe "def" show (perhaps (Some True :: Opt True))
+"True"
+>>> maybe "def" show (perhaps (Some False :: Opt True))
+"False"
+Just False
+
+Demotable datakinds include:
+
+>>> :kind! Demote Symbol
+Demote Symbol :: *
+= Text
+>>> :kind! Demote Natural
+Demote Natural :: *
+= Natural
+>>> :kind! Demote Char
+Demote Char :: *
+= Char
+>>> :kind! Demote Bool
+Demote Bool :: *
+= Bool
+>>> :kind! Demote [k]
+Demote [k] :: *
+= [Demote k]
+>>> :kind! Demote String
+Demote String :: *
+= [Char]
+>>> :kind! Demote (Either k j)
+Demote (Either k j) :: *
+= Either (Demote k) (Demote j)
+>>> :kind! Demote (k,j)
+Demote (k,j) :: *
+= (Demote k, Demote j)
+
+Because there is no promoted `Integer` and `Rational` datakinds,
+this module defines them as `Z` and `Q`.
+
+>>> :kind! Demote Z
+Demote Z :: *
+= Integer
+>>> :kind! Demote Q
+Demote Q :: *
+= Ratio Integer
+
+The `Opt` type comes with
+`Num`, `Integral`, `Fractional`, and `Real` instances,
+using `definite` values to do arithmetic;
+`IsString` and `IsList` instances,
+which let you use literals to construct `Opt`.
+
+>>> "text" :: Opt "hello"
+Some "text"
+>>> "string" :: Opt ['a','b','c']
+Some "string"
+>>> [1, 2] :: Opt ('[] :: [Natural])
+Some [1,2]
+>>> -5 :: Opt (Pos 0)
+Some (-5)
+>>> 3/4 :: Opt (Neg 7 :% 11 :: Q)
+Some (3 % 4)
+-}
+
 {-# LANGUAGE
 ConstraintKinds
 , DataKinds
@@ -16,16 +98,18 @@ ConstraintKinds
 #-}
 
 module Data.Default.Singletons
-  ( Opt (..)
+  ( -- | Optional Datatype
+    Opt (..)
   , SingDef
   , optionally
   , definite
   , perhaps
+    -- | Promoted Datakinds
   , Z (..)
   , Neg
   , Q (..)
-  , SRational (..)
   , SInteger (..)
+  , SRational (..)
   ) where
 
 import Control.Applicative
@@ -37,15 +121,17 @@ import Data.Singletons
 import Data.String
 import Prelude.Singletons ()
 
+{- |
+`Opt`ional type with either
+a default value `Def`,
+specified at the type level by @def@,
+or a specific value `Some`.
+-}
 data Opt (def :: k) where
-  Def
-    :: (SingDef def)
-    => Opt (def :: k)
-  Some
-    :: (SingDef def)
-    => Demote k
-    -> Opt (def :: k)
+  Def :: SingDef def => Opt (def :: k)
+  Some :: SingDef def => Demote k -> Opt (def :: k)
 
+{- | Constraint required to `demote` @@def@. -}
 type SingDef (def :: k) = (SingI def, SingKind k)
 
 instance Semigroup (Opt (def :: k)) where
@@ -97,22 +183,56 @@ instance (SingDef def, IsList (Demote k))
     fromListN n xs = Some $ fromListN n xs
     toList x = toList $ definite x
 
+{- |
+Constructs an `Opt` from a `Maybe`.
+`Nothing` maps to `Def`,
+and `Just` maps to `Some`.
+-}
 optionally :: SingDef def => Maybe (Demote k) -> Opt (def :: k)
 optionally = maybe Def Some
 
+{- |
+Deconstructs an `Opt` to a `Demote`d value.
+`Def` maps to `demote` @@def@,
+and `Some` maps to its argument.
+-}
 definite :: forall k def. Opt (def :: k) -> Demote k
 definite = \case
   Def -> demote @def
   Some a -> a
 
+{- |
+Deconstructs an `Opt` to an `Alternative` `Demote`d value.
+`Def` maps to `empty`,
+and `Some` maps to `pure`.
+-}
 perhaps :: Alternative m => Opt (def :: k) -> m (Demote k)
 perhaps = \case
   Def -> empty
   Some a -> pure a
 
+{- |
+Datakind `Z`, promoting `Integer`,
+
+>>> :kind! Demote Z
+Demote Z :: *
+= Integer
+
+with `Pos` for constructing nonnegative integer types,
+and `Neg` for constructing nonpositive integer types.
+
+>>> demote @(Pos 90210)
+90210
+>>> demote @(Neg 5)
+-5
+>>> demote @(Neg 0)
+0
+
+-}
 data Z = Pos Natural | NegOneMinus Natural
   deriving (Eq, Ord, Read, Show)
 
+{- | Type family for negating a `Natural`.-}
 type family Neg n where
   Neg 0 = Pos 0
   Neg n = NegOneMinus (n - 1)
@@ -146,6 +266,7 @@ instance Num Z where
     then Pos (fromInteger x)
     else NegOneMinus (fromInteger (negate (1 + x)))
 
+{- | Singleton representation for the `Z` kind. -}
 data SInteger (n :: Z) where
   SPos :: SNat n -> SInteger (Pos n)
   SNegOneMinus :: SNat n -> SInteger (NegOneMinus n)
@@ -165,6 +286,21 @@ instance KnownNat n => SingI (Pos n) where
 instance KnownNat n => SingI (NegOneMinus n) where
   sing = SNegOneMinus sing
 
+{- |
+Datakind `Q`, promoting `Rational`,
+
+>>> :kind! Demote Q
+Demote Q :: *
+= Ratio Integer
+
+with `:%` for constructing rational types.
+
+>>> demote @(Pos 7 :% 11)
+7 % 11
+>>> demote @(Neg 4 :% 2)
+(-2) % 1
+
+-}
 data Q = (:%) Z Nat
   deriving (Eq, Ord, Show, Read)
 
@@ -185,6 +321,7 @@ instance Num Q where
   x - y = fromRational (toRational x - toRational y)
   fromInteger x = fromRational (fromInteger x)
 
+{- | Singleton representation for the `Q` kind. -}
 data SRational (n :: Q) where
   SRational :: SInteger n -> SNat m -> SRational (n :% m)
 
